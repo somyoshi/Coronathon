10cb62dcd2af13ec21ca383ef1cba3c23e089b97
Bruno Costa <somyoshi@gmail.com>
2020-06-14 01:58:33 -0300

    ok

--- src/app/screens/home/intro/stepper1/stepper1.component.ts
+++ src/app/screens/home/intro/stepper1/stepper1.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Stepper1Component implements OnInit {
 
-  nome:string ='Bruno Costa';
+  nome ='Bruno Costa';
 
 
   constructor() { }
